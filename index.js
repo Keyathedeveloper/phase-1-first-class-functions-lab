53885c87a81d83a67cd161e81af35cf655e8df1d
TaKeya McFadden <takeyamcfadden@gmail.com>
2024-01-10 21:34:39 -0500

    final commit all test passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return fare * int;
+    }
+}
+const fareMultiplier = createFareMultiplier(4);
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arryofDrivers, func) {
+    return func(arryofDrivers);
+}
